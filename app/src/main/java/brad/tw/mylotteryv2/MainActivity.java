48af13ffa2a27bb28db5512334ab034d9fa64bfa
Brad <brad@brad.tw>
2016-09-20 20:31:08 +0800

    v3

--- app/src/main/java/brad/tw/mylotteryv2/MainActivity.java
+++ app/src/main/java/brad/tw/mylotteryv2/MainActivity.java
@@ -2,6 +2,8 @@ package brad.tw.mylotteryv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void createLottery(View v){
+        Log.d("brad","OK");
+    }
+
 }
