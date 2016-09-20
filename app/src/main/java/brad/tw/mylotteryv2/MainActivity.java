4d528ba53e7160d1d702c5b04b0878ea8e76e5f4
Brad <brad@brad.tw>
2016-09-20 20:46:35 +0800

    v4

--- app/src/main/java/brad/tw/mylotteryv2/MainActivity.java
+++ app/src/main/java/brad/tw/mylotteryv2/MainActivity.java
@@ -4,17 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mesg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mesg = (TextView)findViewById(R.id.textLottery);
     }
 
     public void createLottery(View v){
-        Log.d("brad","OK");
+        Log.d("brad", "OK");
+        mesg.setText("Lottery:" + (int)(Math.random()*49+1));
+
     }
 
 }
